a436f6f74066507164de3b5e218e159aea553f27
aksi1984 <pablo_xmen@op.pl>
2023-11-16 09:00:23 +0100

    Added helper functions for moving items in a list

--- utils.hpp
+++ utils.hpp
@@ -4,6 +4,7 @@
 
 #include <QLineEdit>
 #include <QListWidget>
+#include <QPushButton>
 
 inline bool isLineEditNotEmpty(QLineEdit* lineEdit)
 {
@@ -22,5 +23,43 @@ inline bool isListWidgetItemSelected(QListWidget* listWidget)
     return !listWidget->selectedItems().isEmpty();
 }
 
+///////////////////////////////////////////////////////////////////////
+
+inline void moveItemUp(QListWidget* listWidget)
+{
+    QListWidgetItem* currentItem = listWidget->currentItem();
+    int currentIndex = listWidget->row(currentItem);
+
+    QListWidgetItem* previous = listWidget->item(listWidget->row(currentItem) - 1);
+    int previousIndex = listWidget->row(previous);
+
+    QListWidgetItem* temp = listWidget->takeItem(previousIndex);
+    listWidget->insertItem(currentIndex, temp);
+    listWidget->insertItem(previousIndex, currentItem);
+}
+
+inline void moveItemDown(QListWidget* listWidget)
+{
+    QListWidgetItem* currentItem = listWidget->currentItem();
+    int currentIndex = listWidget->row(currentItem);
+
+    QListWidgetItem* next = listWidget->item(listWidget->row(currentItem) + 1);
+    int nextIndex = listWidget->row(next);
+
+    QListWidgetItem* temp = listWidget->takeItem(nextIndex);
+    listWidget->insertItem(currentIndex, temp);
+    listWidget->insertItem(nextIndex, currentItem);
+}
+
+////////////////////////////////////////////////////////////
+
+inline void updateUpDownButtonsState(QPushButton* upButton, QPushButton* downButton, QListWidget* listWidget)
+{
+    bool upButtonEnabled = listWidget->currentRow() != 0 ? true : false;
+    bool downButtonEnabled = listWidget->currentRow() != listWidget->count() - 1 ? true : false;
+
+    upButton->setEnabled(upButtonEnabled);
+    downButton->setEnabled(downButtonEnabled);
+}
 
 #endif // UTILS_HPP
